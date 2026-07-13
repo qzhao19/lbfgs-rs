3bc4f6150a330dc07e845845158b205688cf690b
qzhao19 <qi.zhao@outlook.fr>
2026-07-13 23:39:37 +0800

    add primitives.rs

---
+++ src/shared/types/primitives.rs
@@ -0,0 +1,17 @@
+// Compile time precision selection
+#[cfg(feature = "f32")]
+pub type ScalarType = f32;
+
+
+#[cfg(feature = "f64")]
+pub type ScalarType = f64;
+
+#[cfg(not(any(feature = "f32", feature = "f64")))]
+compile_error!(
+    "lbfgs-rs supports single (f32) and double (f64) precision floating-point formats exclusively. \
+     Use `cargo build --features f32` or `cargo build --features f64`."
+);
+
+// Features numeric type and label numeric type
+pub type FeatureType = ScalarType;
+pub type LabelType = ScalarType;
