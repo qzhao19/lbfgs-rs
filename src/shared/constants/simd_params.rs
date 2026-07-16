4baddd5307917afecd50926f720c86b47b3514a3
qzhao19 <qi.zhao@outlook.fr>
2026-07-16 23:28:58 +0800

    add simd_params.rs

---
+++ src/shared/constants/simd_params.rs
@@ -0,0 +1,4 @@
+pub const FTYPE_LANES: usize = 4; // 128-bit / 32-bit = 4
+pub const FTYPE_UNROLL: usize = 4;
+pub const DTYPE_LANES: usize = 2; // 128-bit / 64-bit = 2
+pub const DTYPE_UNROLL: usize = 2;
