d112a4a8856adfe323d3445cbb925b089dcbc6a8
qzhao19 <qi.zhao@outlook.fr>
2026-07-22 23:44:49 +0800

    update

--- src/algorithm/linesearch/mod.rs
+++ src/algorithm/linesearch/mod.rs
@@ -1,3 +1,6 @@
 pub mod backtracking;
 pub mod bracketing;
 pub mod linesearch;
+pub use backtracking::backtracking_linesearch::BacktrackingLineSearch;
+pub use bracketing::bracketing_linesearch::BracketingLineSearch;
+pub use linesearch::LineSearch;
