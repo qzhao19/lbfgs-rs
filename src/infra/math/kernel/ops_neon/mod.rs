88d48fc2c3c3311943bb28cf4d10c823d2a76887
qzhao19 <qi.zhao@outlook.fr>
2026-07-25 22:06:05 +0800

    update

--- src/infra/math/kernel/ops_neon/mod.rs
+++ src/infra/math/kernel/ops_neon/mod.rs
@@ -1,4 +1,17 @@
+pub mod vec_diff;
 pub mod vec_dot;
+pub mod vec_ncpy;
+pub mod vec_norm2;
 pub mod vec_scale;
+pub mod vec_scale_inplace;
 pub mod vec_scaled_add;
 pub mod vec_scaled_add_inplace;
+
+pub use vec_diff::vec_diff;
+pub use vec_dot::vec_dot;
+pub use vec_ncpy::vec_ncpy;
+pub use vec_norm2::vec_norm2;
+pub use vec_scale::vec_scale;
+pub use vec_scale_inplace::vec_scale_inplace;
+pub use vec_scaled_add::vec_scaled_add;
+pub use vec_scaled_add_inplace::vec_scaled_add_inplace;
