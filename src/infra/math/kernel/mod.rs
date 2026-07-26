9f36118ae9631428b45cc8f968d449e9a2df68f9
qzhao19 <qi.zhao@outlook.fr>
2026-07-26 22:19:55 +0800

    update

--- src/infra/math/kernel/mod.rs
+++ src/infra/math/kernel/mod.rs
@@ -1,5 +1,1 @@
 pub mod ops_neon;
-pub use ops_neon::vec_dot::vec_dot;
-pub use ops_neon::vec_scale::vec_scale;
-pub use ops_neon::vec_scaled_add::vec_scaled_add;
-pub use ops_neon::vec_scaled_add_inplace::vec_scaled_add_inplace;
