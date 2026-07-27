2e80ed1289ee72db11d59985741d7e502741208a
qzhao19 <qi.zhao@outlook.fr>
2026-07-27 23:30:37 +0800

    remove

--- src/shared/parameter/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod lbfgs;
-pub mod linesearch;
-pub use lbfgs::LbfgsParams;
-pub use lbfgs::LineSearchPolicy;
-pub use lbfgs::LossType;
-pub use linesearch::LineSearchCondition;
-pub use linesearch::LineSearchParam;
