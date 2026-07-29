e4f76628fee4b198911dcf77677e25602cc86be4
qzhao19 <qi.zhao@outlook.fr>
2026-07-29 23:02:59 +0800

    update code

--- src/algorithm/linesearch/mod.rs
+++ src/algorithm/linesearch/mod.rs
@@ -1,6 +1,7 @@
 pub mod backtracking;
 pub mod bracketing;
 pub mod linesearch;
-pub use backtracking::backtracking_linesearch::BacktrackingLineSearch;
-pub use bracketing::bracketing_linesearch::BracketingLineSearch;
-pub use linesearch::LineSearch;
+
+// ── Test module ──
+#[cfg(test)]
+mod tests;
