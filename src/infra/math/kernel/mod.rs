bf2346ad3d0c2f2fee1a10daf1236607cd95cb1d
qzhao19 <qi.zhao@outlook.fr>
2026-07-30 15:42:32 +0800

    update

--- src/infra/math/kernel/mod.rs
+++ src/infra/math/kernel/mod.rs
@@ -1,1 +1,12 @@
-pub mod ops_neon;
+pub mod neon;
+pub mod vec_ops;
+// pub mod vec_diff;
+// pub mod vec_dot;
+// pub mod vec_ncpy;
+// pub mod vec_norm2;
+// pub mod vec_scale_inplace;
+// pub mod vec_scaled_add;
+// pub mod vec_scaled_add_inplace;
+
+#[cfg(test)]
+mod tests;
