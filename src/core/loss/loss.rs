372e5fc03db4a8a6785e9319f548530e49b6c39f
qzhao19 <qi.zhao@outlook.fr>
2026-07-31 21:07:15 +0800

    rename

--- src/core/loss/loss.rs
+++ src/core/loss/loss.rs
@@ -1,7 +1,7 @@
 use crate::data::dataset::Dataset;
 use crate::shared::numeric::ScalarType;
 
-pub trait LossFunc {
+pub(crate) trait LossFunc {
     /// Compute total loss and accumulate gradients over the entire dataset.
     /// Returns total loss value.
     fn evaluate_with_gradient(
